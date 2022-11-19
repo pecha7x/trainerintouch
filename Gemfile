53eea1842418dbb1e0c196af90b205d6f773b80d
pecha7x <pecha7x@gmail.com>
2022-11-19 23:26:05 +0300

    Add Devise gem and support things

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.1'
 
 gem 'bootsnap', require: false
+gem 'devise', '~> 4.8'
 gem 'importmap-rails'
 gem 'jbuilder'
 gem 'pg'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get 'users', to: 'devise/sessions#new'
+  end
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'application#index'
 end
