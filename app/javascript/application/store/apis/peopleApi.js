c8d03d798abacfd17ad3c11e4efd3ef806fa3140
pecha7x <pecha7x@gmail.com>
2022-12-09 20:21:44 +0300

    Store refactoring

--- app/javascript/application/store/apis/peopleApi.js
+++ app/javascript/application/store/apis/peopleApi.js
@@ -3,17 +3,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const PeopleApi = createApi({
   reducerPath: 'people',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000',
-    fetchFn: async (...args) => {
-      return fetch(...args);
-    },
+    baseUrl: 'http://localhost:3000'
   }),
   endpoints(builder) {
     return {
       removePerson: builder.mutation({
-        invalidatesTags: (result, error, person) => {
-          return [{ type: 'Person', id: person.id }];
-        },
         query: (person) => {
           return {
             url: `/people/${person.id}`,
@@ -22,9 +16,6 @@ const PeopleApi = createApi({
         },
       }),
       addPerson: builder.mutation({
-        invalidatesTags: (result, error, user) => {
-          return [{ type: 'People', id: user.id }];
-        },
         query: (user) => {
           return {
             url: '/people',
@@ -37,13 +28,6 @@ const PeopleApi = createApi({
         },
       }),
       fetchPeople: builder.query({
-        providesTags: (result, error, user) => {
-          const tags = result.map((person) => {
-            return { type: 'Person', id: person.id };
-          });
-          tags.push({ type: 'People', id: user.id });
-          return tags;
-        },
         query: (user) => {
           return {
             url: '/people',
