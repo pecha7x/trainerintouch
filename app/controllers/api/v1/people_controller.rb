a19211eccbebe264ee022d7573b48c421648bb0b
pecha7x <pecha7x@gmail.com>
2022-12-10 18:21:38 +0300

    Create People API endpoint

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -2,8 +2,27 @@ module Api
   module V1
     class PeopleController < Api::V1::BaseController
       def index
-        people = Person.all
-        success_response(PersonSerializer.render_as_hash(people, view: :list))
+        resource_collection = resource_class.all
+        success_response(PersonSerializer.render_as_hash(resource_collection, view: :list))
+      end
+
+      def create
+        resource = resource_class.new(resource_params)
+        if resource.save
+          success_response(PersonSerializer.render_as_hash(resource, view: :show))
+        else
+          fail_response(resource.errors.full_messages)
+        end
+      end
+
+      private
+
+      def resource_class
+        Person
+      end
+
+      def resource_params
+        params.permit(:name, :phone, :email, :gender, :dob)
       end
     end
   end
--- spec/requests/api/v1/people_spec.rb
+++ spec/requests/api/v1/people_spec.rb
@@ -18,4 +18,30 @@ RSpec.describe 'Api::V1::People' do
       expect(json['payload'].pluck('id')).to match_array(people.map(&:id))
     end
   end
+
+  describe 'POST /create' do
+    let(:params) do
+      {
+        name: 'John Smith',
+        phone: '+1(223)235-12-21',
+        email: 'johnsmith@gmail.com',
+        gender: 0,
+        dob: '19-04-1988'
+      }
+    end
+
+    before { post api_v1_people_path, params: params }
+
+    it 'executes success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a new created person' do
+      expect(json['payload']).to be_present
+    end
+
+    it 'returns a new created person ID' do
+      expect(json['payload']['id']).to be_present
+    end
+  end
 end
