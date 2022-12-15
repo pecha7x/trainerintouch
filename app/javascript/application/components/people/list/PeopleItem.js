120cf92173c3030749df9b3bfa79af67066bcbd4
pecha7x <pecha7x@gmail.com>
2022-12-15 21:29:01 +0300

    Open Edit person on click a person row

--- app/javascript/application/components/people/list/PeopleItem.js
+++ app/javascript/application/components/people/list/PeopleItem.js
@@ -5,17 +5,21 @@ import { withRouter } from '../../../withRouter';
 function PeopleItem(props) {
   const person = props.person;
 
+  const handleOnDelete = (event) => {
+    event.stopPropagation();
+    props.navigate(`/people/delete/${person.id}`);
+  };
+
   const renderActions = () => {
     return html`
       <div className="flex justify-end">
-        <${Button} className='mr-2' onClick=${() => props.navigate(`/people/edit/${person.id}`)} primary outline>Edit<//>
-        <${Button} className='mr-2' onClick=${() => props.navigate(`/people/delete/${person.id}`)} danger outline>Delete<//>
+        <${Button} className='mr-2' onClick=${(e) => handleOnDelete(e) } danger outline>Delete<//>
       </div>
     `;    
   };
 
   return html`
-    <tr key=${person.id} className="bg-white border-b">
+    <tr key=${person.id} onClick=${() => props.navigate(`/people/edit/${person.id}`)} className="bg-white border-b">
       <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${person.name}</td>
       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">${person.phone}</td>
       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">${person.status}</td>
