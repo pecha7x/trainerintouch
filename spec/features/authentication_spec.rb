d061eeb7840a1e51b4c3a4bd676cd2fee55836e3
pecha7x <pecha7x@gmail.com>
2022-12-18 19:23:48 +0300

    Tests for "Sign Up" process

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe 'Authentication' do
-  context 'Sign In' do
-    context 'The main page' do
+  context 'when an user try to Sign In' do
+    context 'when this goes from the main page' do
       before { visit root_path }
 
       it 'loads the main page and link to "Sign In"' do
@@ -15,18 +15,20 @@ describe 'Authentication' do
       end
     end
 
-    context 'Sign In process' do
-      let!(:user)    { create(:user, email: email, password: password) }
+    context 'when opens "Sign In" form' do
       let(:email)    { 'john@doe.com' }
       let(:password) { 'password7X!' }
 
-      before { visit new_user_session_path }
+      before do
+        create(:user, email: email, password: password)
+        visit new_user_session_path
+      end
 
       it 'loads "Sign Up" link' do
         expect(page).to have_link('sign up', href: new_user_registration_path)
       end
 
-      it 'loads "Sign Up" link' do
+      it 'loads "Forgot password" link' do
         expect(page).to have_link('Forgot your password?', href: new_user_password_path)
       end
 
@@ -37,6 +39,34 @@ describe 'Authentication' do
 
         expect(page).to have_css('#dashboard-app')
       end
+    end
+  end
+
+  context 'when opens "Sign Up" form' do
+    let(:first_name) { 'John' }
+    let(:last_name)  { 'Smith' }
+    let(:email)      { 'john@smith.com' }
+    let(:password)   { 'password7X!' }
+
+    before { visit new_user_registration_path }
+
+    it 'loads "Sign In" link' do
+      expect(page).to have_link('sign in', href: new_user_session_path)
+    end
+
+    it 'loads "Check Confirm Instructions" link' do
+      expect(page).to have_link('Didn\'t receive confirmation instructions?', href: new_user_confirmation_path)
+    end
+
+    it 'login the user to his account' do
+      fill_in 'First name',            with: first_name
+      fill_in 'Last name',             with: last_name
+      fill_in 'Email address',         with: email
+      fill_in 'Password',              with: password
+      fill_in 'Password confirmation', with: password
+      click_on 'Sign up'
+
+      expect(page).to have_text('A message with a confirmation link has been sent to your email address')
     end
   end
 end
