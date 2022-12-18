5dae58a63a7308e8fbfaf451cb576c58cd069985
pecha7x <pecha7x@gmail.com>
2022-12-18 13:43:20 +0300

    Make rubocop happy

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-feature 'Authentication' do
+describe 'Authentication' do
   context 'Sign In' do
     context 'The main page' do
-      background { visit root_path }
-  
-      scenario 'loads the main page and link to "Sign In"' do
+      before { visit root_path }
+
+      it 'loads the main page and link to "Sign In"' do
         expect(page).to have_link('Sign In', href: new_user_session_path)
-      end  
-    
-      scenario '"Sign In" link opens the form' do
+      end
+
+      it '"Sign In" link opens the form' do
         click_link('Sign In')
         expect(page).to have_css('form#new_user')
       end
@@ -20,17 +20,17 @@ feature 'Authentication' do
       let(:email)    { 'john@doe.com' }
       let(:password) { 'password7X!' }
 
-      background { visit new_user_session_path }
-  
-      scenario 'loads "Sign Up" link' do
+      before { visit new_user_session_path }
+
+      it 'loads "Sign Up" link' do
         expect(page).to have_link('sign up', href: new_user_registration_path)
       end
-  
-      scenario 'loads "Sign Up" link' do
+
+      it 'loads "Sign Up" link' do
         expect(page).to have_link('Forgot your password?', href: new_user_password_path)
       end
 
-      scenario 'login the user to his account' do
+      it 'login the user to his account' do
         fill_in 'Email address', with: email
         fill_in 'Password', with: password
         click_on 'Log in'
